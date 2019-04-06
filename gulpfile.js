ce1db71b11696bc9183b19f10f305d8f1cb4ca3c
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2019-04-06 17:17:11 -0400

    feat: CSS Optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
+/**
+ * @file Udacity Web Tooling and Automation Course (subcourse for FEND)
+ * @see Login to Classroom for links to work
+ * @tutorial https://classroom.udacity.com/courses/ud892
+ */
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-
-/**,
+//CSS Optimization
+const watch = require('gulp-watch');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+/**
 *@description Default task
 */
 gulp.task('default', done => {
-  // TODO: add watch task for scss files
-
+  gulp.watch('sass/**/*.scss', gulp.series('styles'));
   browserSync.init({
     server: './'
   })
@@ -14,11 +21,23 @@ gulp.task('default', done => {
 })
 
 /**
-*@description Styles task
-*/
+ * @function Styles task
+ * @description Create sass and auto-prefix for 2 most recent browser versions
+ * @tutorial https://classroom.udacity.com/courses/ud892/lessons/5332430837/concepts/53252207850923
+ * @tutorial https://classroom.udacity.com/courses/ud892/lessons/5332430837/concepts/53252207860923
+ * @tutorial https://classroom.udacity.com/courses/ud892/lessons/5332430838/concepts/55096392250923
+ */
 gulp.task('styles', done => {
   gulp
-  .src('index.html')
-  .pipe(browserSync.stream())
+    .src("sass/**/*.scss")
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
   done();
-})
+});
