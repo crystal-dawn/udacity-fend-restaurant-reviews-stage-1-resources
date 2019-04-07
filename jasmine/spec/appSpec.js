8887776a895686773868eb6d642dc7df5861004b
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2019-04-06 21:12:50 -0400

    feat: Criteria card section testing

--- jasmine/spec/appSpec.js
+++ jasmine/spec/appSpec.js
@@ -1,28 +1,38 @@
 $(function() {
-    describe('Page', () => {
-      it('has a header', () => {
-        const element = document.querySelector('#pageHeader');
+  describe('Page', () => {
+    it('has a header', () => {
+      const element = document.querySelector('#pageHeader');
 
-        expect(element).not.toBe(null);
-      });
-      it('has a top navigation menu', () => {
-        const element = document.querySelector('#topNav');
-
-        expect(element).not.toBe(null);
-      });
-      it('has a bottom navigation menu', () => {
-        const element = document.querySelector('#bottomNav');
-
-        expect(element).not.toBe(null);
-      });
-      it('has a main element', () => {
-        const element = document.querySelector('#criterias');
-
-        expect(element).not.toBe(null);
-      });
+      expect(element).not.toBe(null);
     });
+    it('has a top navigation menu', () => {
+      const element = document.querySelector('#topNav');
 
-    describe('Main element', () => {
-
+      expect(element).not.toBe(null);
     });
+    it('has a bottom navigation menu', () => {
+      const element = document.querySelector('#bottomNav');
+
+      expect(element).not.toBe(null);
+    });
+  });
+
+  describe('Main element', () => {
+    it('exists', () => {
+      const element = document.querySelector('#criterias');
+
+      expect(element).not.toBe(null);
+    });
+    it('has at least one criteria card', () => {
+      const criteriaCard = document.querySelector('.criteria').children.length;
+      expect(criteriaCard).toBeGreaterThan(0);
+    });
+  });
+
+  describe('Section element', () => {
+    it('has a header tag', () => {
+      const sectionHeader = document.querySelectorAll('.criteria .criteria-header');
+      expect(sectionHeader).not.toBe(null);
+    })
+  });
 }());
--- js/app.js
+++ js/app.js
@@ -16,4 +16,34 @@ const mainElement = () => {
 }
 
 
+/**
+ * @description Create one criteria card for each item from the rubric
+ */
+function criteriaCard() {
+  //create a Set for criterias. Sets automatically don't add duplicate values
+  //without sending an error. This creates one list with a single entry for
+  //any criteria listed in resources
+  let criteriaSet = new Set();
+  resources.forEach(criteria => criteriaSet.add(criteria.criteria));
+
+  const entries = criteriaSet.entries();
+
+  //one card for each criteria from the array
+  for (const [criteriaIndex, criteria] of entries) {
+    document.querySelector('main').insertAdjacentHTML('afterbegin',
+      `<section class="criteria">
+          <div id="ctn${criteriaIndex}" style="position:relative;">
+            <a id="criteria${criteriaIndex}">&nbsp;</a>
+            <h2 class="criteria-header">${criteria}</h2>
+          </div>
+        </section>`);
+
+    //populate Rubric criteria dropdown nav
+    document.querySelector('ul').insertAdjacentHTML('afterbegin',
+      `<li class="criteria-list">
+         <a class="dropdown-link" href="#criteria${criteriaIndex}">${criteria}</a>
+      </li>`)
+    typeCard(criteria, criteriaIndex);
+  }
+}
 document.body.onload = loadPage();
